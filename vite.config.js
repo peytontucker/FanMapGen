6b5101cb7384b814d96ca0441a7a1a77773f18eb
peytontucker <peytonmtucker@gmail.com>
2023-11-06 08:37:10 -0500

    update config, attempt github pages hosting

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import process from 'node:process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: '/FanMapGen/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/FanMapGen/' : '/'
 })
